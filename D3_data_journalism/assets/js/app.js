9149469f31401c2c020a034148006c529bc050d3
Eliot Chern <eliotchern@gmail.com>
2021-02-11 04:32:59 -0800

    setup svg parameters

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,21 @@
 // @TODO: YOUR CODE HERE!
+
+// Setup svg img parameters
+var svgWidth = 800;
+var svgHeight = 560;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+
+// Import Data
+var dataFile = "assets/data/data.csv"
+
+d3.csv(dataFile).then(function)
